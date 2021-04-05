e5489f838f67241213a682861445d1bb58b51129
Miguel Rios <miguelriosoliveira@gmail.com>
2021-04-04 22:22:12 -0300

    Global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import { AppProps } from "next/app";
+import { AppProps } from 'next/app';
+
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	// eslint-disable-next-line react/jsx-props-no-spreading
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
 	return (
 		<>
 			<Head>
 				<title>ig.news | Home</title>
 			</Head>
-			<h1 className={styles.title}>hellou</h1>
+			<h1>hellou</h1>
 		</>
 	);
 }
