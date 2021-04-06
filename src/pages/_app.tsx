644bc48f2d0c3be798e1fcd9439e229533a22a0f
Miguel Rios <miguelriosoliveira@gmail.com>
2021-04-05 22:53:31 -0300

    Created Header component and added Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import { AppProps } from 'next/app';
 
+import { Header } from '../components/Header';
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	// eslint-disable-next-line react/jsx-props-no-spreading
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Header />
+			{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
