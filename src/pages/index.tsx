a2df9521a8bdedf18cef2bd45337aa4eb2862a88
Miguel Rios <miguelriosoliveira@gmail.com>
2021-04-10 21:16:21 -0300

    SSG using the stripe API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -23,7 +23,7 @@ export default function Home({ product }: Props) {
 
 			<main className={styles.contentContainer}>
 				<section className={styles.hero}>
-					<span>👏 Hey, welcome</span>
+					<span>👏 Hey, welcome!</span>
 					<h1>
 						New about the <span>React</span> world.
 					</h1>
@@ -40,7 +40,7 @@ export default function Home({ product }: Props) {
 	);
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 	const price = await stripe.prices.retrieve('price_1IepvWEPP6KthwN1yTriHSdI');
 
 	const product = {
@@ -52,5 +52,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
 		props: {
 			product,
 		},
+		revalidate: 60 * 60 * 24, // 24 hours
 	};
 };
